2bd88de678cbd5466377803c58e36c47e5ee3f27
Tonieie <tonzazil12@gmail.com>
2020-07-20 18:25:55 +0700

    Added getX getY isTouching isConnected methods

--- PS4Serial.cpp
+++ PS4Serial.cpp
@@ -64,13 +64,57 @@ bool PS4Serial::getButton(Button button)
     case PS4B_R3:
         return button_right.asBool.bit6;
         break;
+
+    //*****Center Button*****//
     case PS4B_Touch:
-        return button_right.asBool.bit7;
+        return button_center.asBool.bit0;
         break;
 
     default:
+        return NULL;
         break;
     }
+}
+
+bool PS4Serial::isTouching(bool finger)
+{
+    if(finger == 0)
+    {
+        return button_center.asBool.bit1;
+    }
+    else if(finger == 1)
+    {
+        return button_center.asBool.bit2;
+    }
+}
+
+uint16_t PS4Serial::getX(bool finger)
+{
+    if(finger == 0)
+    {
+        return touch_x1.asInt;
+    }
+    else if(finger == 1)
+    {
+        return touch_x2.asInt;
+    }
+}
+
+uint16_t PS4Serial::getY(bool finger)
+{
+    if(finger == 0)
+    {
+        return touch_y1.asInt;
+    }
+    else if(finger == 1)
+    {
+        return touch_y2.asInt;
+    }
+}
+
+bool PS4Serial::isConnected()
+{
+    return button_center.asBool.bit3;
 }
 
 uint8_t PS4Serial::getAnalog(Axis axis)
@@ -114,17 +158,30 @@ void PS4Serial::unpack(void)
         buffer[counter++] = _port->read();
         if (counter == 0 && buffer[0] != '#')
             counter = 0;
-        if ((buffer[counter - 11] == '#') && (buffer[counter - 10] == 's') && (buffer[counter - 1] == '\r') && (buffer[counter] == '\n'))
+        if ((buffer[counter - 20] == '#') && (buffer[counter - 19] == 's') && (buffer[counter - 1] == '\r') && (buffer[counter] == '\n'))
         {
-            joyAnalog_Lx = buffer[counter - 9];
-            joyAnalog_Ly = buffer[counter - 8];
-            joyAnalog_L2 = buffer[counter - 7];
-            joyAnalog_Rx = buffer[counter - 6];
-            joyAnalog_Ry = buffer[counter - 5];
-            joyAnalog_R2 = buffer[counter - 4];
-            button_left.asByte = buffer[counter - 3];
-            button_right.asByte = buffer[counter - 2];
-            break;
+            joyAnalog_Lx = buffer[counter - 18];
+            joyAnalog_Ly = buffer[counter - 17];
+            joyAnalog_L2 = buffer[counter - 16];
+            joyAnalog_Rx = buffer[counter - 15];
+            joyAnalog_Ry = buffer[counter - 14];
+            joyAnalog_R2 = buffer[counter - 13];
+
+            touch_x1.asByte[0] = buffer[counter - 12];
+            touch_x1.asByte[1] = buffer[counter - 11];
+            touch_y1.asByte[0] = buffer[counter - 10];
+            touch_y1.asByte[1] = buffer[counter - 9];
+
+            touch_x2.asByte[0] = buffer[counter - 8];
+            touch_x2.asByte[1] = buffer[counter - 7];
+            touch_y2.asByte[0] = buffer[counter - 6];
+            touch_y2.asByte[1] = buffer[counter - 5];
+
+            button_left.asByte = buffer[counter - 4];
+            button_right.asByte = buffer[counter - 3];
+            button_center.asByte = buffer[counter - 2];
+
+            counter=0;
         }
         if (counter >= (buff_len * 2))
             counter = 0;
